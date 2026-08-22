eb51f33be92cf4afeda7a2b9c9e79291e43579b6
COTRIM349 <matheuscotrim253@gmail.com>
2026-08-21 22:22:47 -0300

    refactor: promover motor hídrico v2 como fonte única

--- modules/water-balance/services/index.ts
+++ modules/water-balance/services/index.ts
@@ -1,5 +1,5 @@
 export * from "./water-balance.service";
-export * from "./pivot-engine";
+export * from "./pivot-engine-v2";
 export * from "./crop-coefficients";
 export * from "./soil-water-balance";
 export * from "./map-hydric-status";
