1f1ebe805b7d2625469a867575750b46f5f17e28
COTRIM349 <matheuscotrim253@gmail.com>
2026-08-23 20:58:06 -0300

    feat(hydric): export dual coefficient utilities

--- modules/water-balance/services/index.ts
+++ modules/water-balance/services/index.ts
@@ -1,5 +1,6 @@
 export * from "./water-balance.service";
 export * from "./pivot-engine-operational";
+export * from "./dual-crop-coefficient";
 export * from "./crop-coefficients";
 export * from "./soil-water-balance";
 export * from "./map-hydric-status";
