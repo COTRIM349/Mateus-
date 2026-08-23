44aa5d995890e97a884027d090135367f75b8ea4
COTRIM349 <matheuscotrim253@gmail.com>
2026-08-22 23:01:51 -0300

    fix: usa guarda operacional como entrada pública do motor

--- modules/water-balance/services/index.ts
+++ modules/water-balance/services/index.ts
@@ -1,5 +1,5 @@
 export * from "./water-balance.service";
-export * from "./pivot-engine-v2";
+export * from "./pivot-engine-operational";
 export * from "./crop-coefficients";
 export * from "./soil-water-balance";
 export * from "./map-hydric-status";
